c58a376b0d969ebd18b987f471432802303bdf83
Kateryna <75367511+katement@users.noreply.github.com>
2025-01-21 16:02:10 +0000

    21/01

--- src/components/ImageCard/ImageCard.jsx
+++ src/components/ImageCard/ImageCard.jsx
@@ -1,11 +1,14 @@
 // import stub from "../../assets/react.svg";
+import s from "../ImageCard/ImageCard.module.css";
+
+
 const ImageCard = ({ image, onClick }) => {
   // const { urls, slug } = image;
   // console.log(results.urls.regular);
   if (image)
     return (
-      <div onClick={() => onClick(image)}>
-        <img src={image?.urls?.small} alt={image.slug ?? "Image"} />
+      <div className={s.card} onClick={() => onClick(image)}>
+        <img  src={image?.urls?.small} alt={image.slug ?? "Image"} />
       </div>
     );
 };
